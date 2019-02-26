927be1450164e553be1ca680b6b3dcb9291b221a
anubhawlenskart <anubhaw.srivastava@lenskart.in>
2019-02-26 18:31:11 +0530

    fix validation issue in otp screen

--- js/otpscreen.js
+++ js/otpscreen.js
@@ -10,16 +10,16 @@ var otpscreen = {
         htmlStr += '<div class="input-box">';
         htmlStr += '<div class="row otp-section">';
         htmlStr += '<div class="col-md-3">';
-        htmlStr += '<input type="text" class="otp-cls" name="otp-1" id="otp-1" placeholder="" class="form-control" maxlength="1" tabindex="1" autofocus>';
+        htmlStr += '<input type="number" class="otp-cls" name="otp-1" id="otp-1" placeholder="" class="form-control" maxlength="1" autofocus>';
         htmlStr += '</div>';
         htmlStr += '<div class="col-md-3">';
-        htmlStr += '<input type="text" class="otp-cls" name="otp-2" id="otp-2" placeholder="" class="form-control" maxlength="1" tabindex="2">';
+        htmlStr += '<input type="number" class="otp-cls" name="otp-2" id="otp-2" placeholder="" class="form-control" maxlength="1">';
         htmlStr += '</div>';
         htmlStr += '<div class="col-md-3">';
-        htmlStr += '<input type="text" class="otp-cls" name="otp-3" id="otp-3" placeholder="" class="form-control" maxlength="1" tabindex="3">';
+        htmlStr += '<input type="number" class="otp-cls" name="otp-3" id="otp-3" placeholder="" class="form-control" maxlength="1">';
         htmlStr += '</div>';
         htmlStr += '<div class="col-md-3">';
-        htmlStr += '<input type="text" class="otp-cls" name="otp-4" id="otp-4" placeholder="" class="form-control" maxlength="1" tabindex="4">';
+        htmlStr += '<input type="number" class="otp-cls" name="otp-4" id="otp-4" placeholder="" class="form-control" maxlength="1">';
         htmlStr += '</div>';
         htmlStr += '</div>';
         htmlStr += '<div class="buttons buttons-static">';
@@ -70,17 +70,21 @@ var otpscreen = {
         });
 
         // Blur event
-        $('.otp-cls').keyup(function(){
-            var idattr = $(this).attr('id');
-            var split = idattr.split('-');
-            var nexttab = parseInt(parseInt(split[1])+1);
+        var numericButtonUnicode = [48,49,50,51,52,53,54,55,56,57];
+        $('.otp-cls').keyup(function(e){
+            var unicode = e.charCode? e.charCode : e.keyCode;           
+            if(numericButtonUnicode.indexOf(unicode) != -1){
+                var idattr = $(this).attr('id');
+                var split = idattr.split('-');
+                var nexttab = parseInt(parseInt(split[1])+1);
 
-            var otp = $('#otp-1').val()+$('#otp-2').val()+$('#otp-3').val()+$('#otp-4').val();
-            $('#otp-'+nexttab).focus().val($('#otp-'+nexttab).val());
-            if(otp.length == 4){
-                $('#otpnextBtn').removeClass('disable');
-            }
-        })
+                var otp = $('#otp-1').val()+$('#otp-2').val()+$('#otp-3').val()+$('#otp-4').val();
+                $('#otp-'+nexttab).focus().val($('#otp-'+nexttab).val());
+                if(otp.length == 4){
+                    $('#otpnextBtn').removeClass('disable');
+                }
+            }            
+        });
       },
 
     SubmitNext : function(mobile,otp){
